2be03f0b4a4394e88d44d8d6786b073967b67378
CarlosCordovaC <CarlosCordovaC@outlook.es>
2023-03-07 17:43:12 -0600

    .

---
+++ 23_03_07_000_Capitulo45_1.py
@@ -0,0 +1,12 @@
+""""import datetime
+
+fecha = datetime.datetime(2023, 3, 7, 17,40,30)
+
+print(fecha)"""
+
+#----------------------------------------------
+#.Solo llamar el año
+import datetime
+fecha = datetime.datetime.now()
+print("Año: ",fecha.year)
+print("Mes: ",fecha.month, "Dia: ",fecha.day)
