ba76ce0f1803ad3b69a3d205eae18eadda47548e
CarlosCordovaC <CarlosCordovaC@outlook.es>
2023-03-09 12:52:30 -0600

    .

---
+++ 23_03_09_000_Capitulo47_1.py
@@ -0,0 +1,8 @@
+import re
+
+texto= "Bienvenidos a programacion"
+busqueda = re.search("i",texto)
+
+
+
+print(busqueda)
---
+++ 23_03_09_000_Capitulo48_1.py
@@ -0,0 +1,10 @@
+import re
+
+texto= "Bienvenidos a programacion"
+#La funcion re.findall funciona para buscar todas las letras o palabras que esten dentro de la busqueda
+busqueda = re.findall("i",texto)
+
+
+
+
+print(busqueda)
